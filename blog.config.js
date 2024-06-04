f45a73c6479dbfd3217be00df7b569044b1f4800
Zhiran Zhang <minerva_z@mail.ustc.edu.cn>
2024-06-04 11:09:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'MY BLOG',
+  author: 'Minerva Zhang',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
